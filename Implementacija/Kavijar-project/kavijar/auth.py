517710dea8eb2a4629205f430ff34e781630b925
pseto-kaiba <39837275+pseto-kaiba@users.noreply.github.com>
2020-05-15 17:27:57 +0200

    login odvojen

--- Implementacija/Kavijar-project/kavijar/auth.py
+++ Implementacija/Kavijar-project/kavijar/auth.py
@@ -23,39 +23,48 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        if request.form['loginuser'] == 'Prijavi se':
-            user = User.query.filter_by(username=username).first()
-            if user is None:
-                error = 'Pogrešno korisničko ime.'
-            elif not check_password_hash(user.password, password):
-                error = 'Pogrešna lozinka.'
-            if error is None:
-                session.clear()
-                session['user_id'] = user.idUser;
-                rolePageDict = {
-                    'I': url_for('index'),
-                    'M': url_for('mod'),
-                    'A': url_for('admin.adminmain')
-                }
-                return redirect(rolePageDict[user.role])
-        elif request.form['loginuser'] == 'Registruj se':
-            error = None
-            if not username:
-                error = 'Korisničko ime ne sme biti prazno.'
-            elif not password:
-                error = 'Lozinka ne sme biti prazna.'
-            elif User.query.filter_by(username=username).first():
-                error = 'Korisnik sa tim imenom već postoji.'
-
-            if error is None:
-                new_user = User(username=username, password=generate_password_hash(password), role='I',
-                                caviar=0, statusUpdate=0, dateUnban=None, dateCharLift=None)
-                db.session.add(new_user)
-                db.session.commit()
-                return redirect(url_for('auth.login'))
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            error = 'Pogrešno korisničko ime.'
+        elif not check_password_hash(user.password, password):
+            error = 'Pogrešna lozinka.'
+        if error is None:
+            session.clear()
+            session['user_id'] = user.idUser;
+            rolePageDict = {
+                'I': url_for('index'),
+                'M': url_for('mod'),
+                'A': url_for('admin.adminmain')
+            }
+            return redirect(rolePageDict[user.role])
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        error = None
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Korisničko ime ne sme biti prazno.'
+        elif not password:
+            error = 'Lozinka ne sme biti prazna.'
+        elif User.query.filter_by(username=username).first():
+            error = 'Korisnik sa tim imenom već postoji.'
+
+        if error is None:
+            new_user = User(username=username, password=generate_password_hash(password), role='I',
+                            caviar=0, statusUpdate=0, dateUnban=None, dateCharLift=None)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        flash(error)
+
+    return render_template('auth/register.html')
 
 
 @bp.before_app_request
