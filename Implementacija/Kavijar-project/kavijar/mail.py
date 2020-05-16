3e9d080c396fcbab6634a7237220fb2e51250203
Tatomirovic <39652686+tatomirovic@users.noreply.github.com>
2020-05-16 23:53:58 +0200

    mali bugfix

--- Implementacija/Kavijar-project/kavijar/mail.py
+++ Implementacija/Kavijar-project/kavijar/mail.py
@@ -17,8 +17,8 @@ bp = Blueprint('mail', __name__, url_prefix='/mail')
 @check_ban
 def msg_main():
     g.user.statusUpdate=0
-    msg_list = Mailmsg.query.filter(idTo=g.user.idUser).all()
-    return render_template('mail/mail.html')
+    msg_list = Mailmsg.query.filter_by(idTo=g.user.idUser).all()
+    return render_template('mail/mail.html',msg_list=msg_list)
 
 
 @bp.route('/send_msg', methods=('GET', 'POST'))
