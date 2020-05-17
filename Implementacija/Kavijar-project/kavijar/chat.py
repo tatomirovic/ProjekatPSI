8d17c17a7ff103928528cb6d8f1f65940520db1f
pseto-kaiba <39837275+pseto-kaiba@users.noreply.github.com>
2020-05-17 02:02:06 +0200

    Chat sistem apdejt
    Sada cuva chat poruke u bazi i grabi zadnjih 15 pri loadovanju /chat stranice

--- Implementacija/Kavijar-project/kavijar/chat.py
+++ Implementacija/Kavijar-project/kavijar/chat.py
@@ -5,20 +5,22 @@ from flask import (
 )
 import datetime;
 from flask_socketio import SocketIO
+from sqlalchemy import desc
 
 from kavijar.auth import login_required, check_ban
-from kavijar.models import User, Mailmsg
+from kavijar.models import User, Chatmsg
 from . import db, socketio
 
 bp = Blueprint('chat', __name__, url_prefix='/chat')
 
 
 @bp.route('/')
+@login_required
 def sessions():
-    return render_template('chat/chat.html')
+    recent_messages = Chatmsg.query.order_by(desc(Chatmsg.time)).limit(15).all()
+    return render_template('chat/chat.html', recent_messages=recent_messages)
 
 def message_received(methods=['GET', 'POST']):
-    #print('message was received!!!')
     #click.echo(f'User name is: {g.user.username}')
     pass
 
@@ -27,6 +29,9 @@ def message_received(methods=['GET', 'POST']):
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     if session['user_id']:
+        new_msg = Chatmsg(idSender=session['user_od'], time=datetime.datetime.now(), content=json.message)
+        db.session.add(new_msg)
+        db.session.commit()
         socketio.emit('my response', json, callback=message_received)
 
 
