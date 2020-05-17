b234fed59f25e5137cbbdacc8ae55a60c1b0a296
flopabez <33830264+flopabez@users.noreply.github.com>
2020-05-17 03:30:12 +0200

    city creation and update functionalities added
    population growth now included in resource update wrapper
    city creation organized as a single function that directly writes to db
    town hall creation added - first of many building factory functions, rought draft
    minor file reorganisation concerning the above functionalities

---
+++ Implementacija/Kavijar-project/kavijar/updateWrappers.py
@@ -0,0 +1,37 @@
+from flask import (
+    g, redirect, url_for
+)
+
+from werkzeug.exceptions import abort
+
+from . import db;
+
+from .models import City, Building
+import functools, datetime
+from . import game_rules as gr
+
+
+##Obavezno koristiti ovaj dekorator pri svakoj modifikaciji tabela city, building, army, trade, recruiting
+def update_resources(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        city = City.query.filter_by(idOwner=g.user.idUser).first()
+        t0 = city.lastUpdate
+        t1 = city.lastUpdate = datetime.datetime.now()
+        dt = (t1 - t0).seconds / 3600
+        lvl = Building.query.filter_by(idCity=city.idCity).first().level
+        city.gold += gr.goldPerHour * city.civilians * dt
+        #TODO obracunati upkeep
+        city.wood += gr.woodPerHour * city.woodworkers * dt
+        city.stone += gr.stonePerHour * city.stoneworkers * dt
+
+        city.population = gr.growth(city.population, dt, lvl)
+        city.civilians = city.population - city.woodworkers - city.stoneworkers
+        
+        db.session.commit()
+        return view(**kwargs)
+
+    return wrapped_view
