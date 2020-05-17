9ac22554ab62a34c76639d8e9a830c441d2878d8
pseto-kaiba <39837275+pseto-kaiba@users.noreply.github.com>
2020-05-17 16:30:01 +0200

    Update game.py

--- Implementacija/Kavijar-project/kavijar/game.py
+++ Implementacija/Kavijar-project/kavijar/game.py
@@ -3,12 +3,21 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from kavijar.auth import login_required
-from . import db;
+from kavijar.auth import login_required, player_required
+from . import db
+
+from .models import City, Building
+import functools, datetime
+from . import updateWrappers
 
 bp = Blueprint('game', __name__)
 
 
 @bp.route('/')
+@player_required
+@updateWrappers.update_resources
 def index():
-    return render_template('game/main_map.html')
+    city_list = City.query.all()
+    return render_template('game/main_map.html', city_list=city_list)
+
+
