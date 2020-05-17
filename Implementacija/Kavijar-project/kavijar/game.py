ac335466bb0750f6aefc7bc369eb1742f6a73a07
botuljina <lsimovic140@gmail.com>
2020-05-17 15:54:12 +0200

    Dodata mapa u main_map.html
    Dodata mapa plus popup window za prikaz svih gradova

--- Implementacija/Kavijar-project/kavijar/game.py
+++ Implementacija/Kavijar-project/kavijar/game.py
@@ -4,18 +4,11 @@ from flask import (
 from werkzeug.exceptions import abort
 
 from kavijar.auth import login_required
-from . import db
-
-from .models import City, Building
-import functools, datetime
-from . import updateWrappers
+from . import db;
 
 bp = Blueprint('game', __name__)
 
 
 @bp.route('/')
-@updateWrappers.update_resources
 def index():
     return render_template('game/main_map.html')
-
-
